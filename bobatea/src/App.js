aef2800476fe51dc05d731d33d7571719b8e4e2e
Dhriti <134310082+parikhdp@users.noreply.github.com>
2026-02-28 05:25:03 +0530

    did basic game container layout setup with soft moving blobs in the bg

--- bobatea/src/App.js
+++ bobatea/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-wrapper">
+      <div className="blob blob-3"></div>
+      <div className="blob blob-4"></div>
+
+      <div className="game-container">
+        <div className="game-content">
+          <h1 className="title">Boba Catch 🧋</h1>
+        </div>
+      </div>
     </div>
   );
 }
